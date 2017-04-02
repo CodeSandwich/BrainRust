c11da0170f71197da1028f8f41c6af217163aedb
CodeSandwich <igor.zuk@protonmail.com>
2017-04-02 18:52:00 +0200

    Use CLAP for CLI

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,37 +1,44 @@
 extern crate brainrust;
+#[macro_use]
+extern crate clap;
 
 use brainrust::{U8Editor, ExecutionError, ParseError, Program};
+use clap::{App, Arg};
 use std::io;
 use std::io::Read;
-use std::env;
 use std::fs::File;
 
 pub fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() != 2 {
-        println!("Expected 1 argument, source code file");
-        return
-    }
-    let mut file = match File::open(&args[1]) {
+    let matches = App::new("Brainrust CLI")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .arg(
+            Arg::with_name("source file")
+            .required(true)
+        )
+        .get_matches();
+    let filename_os_str = matches.value_of_os("source file").unwrap();
+    let filename = matches.value_of_lossy("source file").unwrap();
+    let mut file = match File::open(filename_os_str) {
         Ok(file) => file,
         Err(_) => {
-            println!("Failed to open file {}", &args[1]);
+            println!("Failed to open file {}", &filename);
             return
         }
     };
     let mut source = String::new();
     if let Err(_) = file.read_to_string(&mut source) {
-        println!("Failed to load source from file {}", &args[1]);
+        println!("Failed to load source from file {}", filename);
         return
     }
     let program = match Program::new(&mut source.chars()) {
         Ok(program) => program,
         Err(ParseError::LoopNeverClosed(index)) => {
-            println!("File {}, character {}: unclosed loop", &args[1], index);
+            println!("File {}, character {}: unclosed loop", filename, index);
             return
         }
         Err(ParseError::NonexistentLoopClosed(index)) => {
-            println!("File {}, character {}: closure of never opened loop", &args[1], index);
+            println!("File {}, character {}: closure of never opened loop", filename, index);
             return
         }
     };
